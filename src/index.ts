6cf258b0f2521caff576d8d4a93ff6fa436f48d1
tacotaco0808 <takuto1488@gmail.com>
2025-12-30 23:18:21 +0900

    https対応

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
-import http from "http";
+import https from "https";
+import fs from "fs";
 
 // expressアプリケーションとHTTPサーバーの作成
 const app = express();
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync("certs/server_key.pem"),
+  cert: fs.readFileSync("certs/server_crt.pem"),
+};
+const server =
+  process.env.SERVER_ENV === "dev"
+    ? https.createServer(options, app)
+    : https.createServer(app);
 
 app.get("/", (req, res) => {
   res.send("Hello, WebRTC Signaling Server!");
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = Number(process.env.PORT) || 3000;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+  console.log(`network https://192.168.1.15:${PORT}`);
 });
