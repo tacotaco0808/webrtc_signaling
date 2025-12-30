f2f69dd7570b5a47e871e36c52294c5a591fc726
tacotaco0808 <takuto1488@gmail.com>
2025-12-30 23:02:10 +0900

    bunハローワルド

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!!!");
+import express from "express";
+import http from "http";
+
+// expressアプリケーションとHTTPサーバーの作成
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello, WebRTC Signaling Server!");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
