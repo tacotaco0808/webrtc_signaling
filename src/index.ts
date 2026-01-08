4df97bfa73fc65f3b6046884db4802b29b2ea043
tacotaco0808 <takuto1488@gmail.com>
2026-01-08 15:58:42 +0900

    expressアプリケーションの設定を整理し、HTTPサーバーのオプションを削除。テストクライアントのサーバーURLをlocalhostに変更。

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,9 @@ import http from "http";
 import fs from "fs";
 import { Server } from "socket.io";
 
-// expressアプリケーションとHTTPサーバーの作成
+// expressアプリケーション
 const app = express();
-const options = {
-  key: fs.readFileSync("certs/server_key.pem"),
-  cert: fs.readFileSync("certs/server_crt.pem"),
-};
+
 // nginxを使う場合は通常のHTTPサーバーとして動作
 const server = http.createServer(app);
 const io = new Server(server);
